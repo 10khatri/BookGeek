4194282e8f8bf14acec1074cff92d49428905c92
10khatri <f1footballfreestylers@gmail.com>
2023-06-05 16:00:56 +0530

    testing

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -9,6 +9,7 @@ export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [isGuestLogin, setIsGuestLogin] = useState(false);
+
   useEffect(() => {
     if (isGuestLogin) {
       handleLogin();
